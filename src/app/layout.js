56d0a5c3b86c46f544851233cda408bde5cb4c20
Djumakulov Muhammadali <muhammadalimuhammadali2012@gmail.com>
2025-08-13 14:39:41 +0500

    я

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,12 +1,15 @@
 import { ProductProvider } from '../components/ProductContext';
+import { AuthProvider } from '../components/AuthContext'; // добавим
 
 export default function RootLayout({ children }) {
   return (
     <html lang="ru">
       <body>
-        <ProductProvider>
-          {children}
-        </ProductProvider>
+        <AuthProvider>
+          <ProductProvider>
+            {children}
+          </ProductProvider>
+        </AuthProvider>
       </body>
     </html>
   );
--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom/client"; // ✅ Важно!
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
+   <BrowserRouter>
+    <AuthProvider>
+     <Provider store={store}>
         <App />
       </Provider>
+    </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
