cdae58d0bb97b7edeff1d813636518c30b6a8750
Djumakulov Muhammadali <muhammadalimuhammadali2012@gmail.com>
2025-08-28 13:11:17 +0500

    Fix: remove self-import from SendProductPage

--- src/pages/SendProductPage.js
+++ src/pages/SendProductPage.js
@@ -1,11 +1,7 @@
-import SendProductPage from "./pages/SendProductPage";
-
-
 export default function SendProductPage() {
   return (
     <div className="p-10">
       <h1 className="text-2xl font-bold">Send Product Page</h1>
-      <p>Here you can add new products.</p>
     </div>
   );
 }
